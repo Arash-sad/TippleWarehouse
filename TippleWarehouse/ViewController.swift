6e85d0f9e4150a071ccba082c523078f1be6a56d
DrArash <arash_part2@yahoo.com>
2016-10-16 18:14:21 +1100

    Initial Commit

---
+++ TippleWarehouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TippleWarehouse
+//
+//  Created by Arash Sadeghieh E on 16/10/2016.
+//  Copyright © 2016 Treepi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
